065456a223b6a8095c5617e55513d40e132a49b3
Kanchan Barsainya <kanchan.barsainya@cvscaremark.com>
2020-03-07 13:18:51 -0500

    added routing for profile list.

--- apps/monorepofun/src/app/app.module.ts
+++ apps/monorepofun/src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { ProfileListEffects } from './../../../../libs/profile-list-store/profile-list.effects';
+import { ProfileEffects } from './../../../../libs/profile-store/profile.effects';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { UiModule } from '../../../../libs/ui/src';
 import { StoreModule } from '@ngrx/store';
@@ -11,6 +12,11 @@ import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { InfoComponent } from './info.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ProfileListReducer } from 'libs/profile-list-store/profile-list.reducers'
+import { RandomUserReducer } from 'libs/profile-store/profile.reducers'
+
+
 
 @NgModule({
   declarations: [AppComponent, InfoComponent],
@@ -18,12 +24,21 @@ import { InfoComponent } from './info.component';
     BrowserModule,
     UiModule,
     MatToolbarModule,
+    HttpClientModule,
     RouterModule.forRoot(
       [
         {
           path: '',
           pathMatch: 'full',
           component: InfoComponent
+        },
+        {
+          path: 'profile-list',
+          pathMatch: 'full',
+          loadChildren: () =>
+            import('../../../../libs/feature-profile-grid/feature-profile-grid.module').then(
+              module => module.FeatureProfileGridModule
+            )
         },
         {
           path: 'profile-details',
@@ -37,7 +52,8 @@ import { InfoComponent } from './info.component';
       { initialNavigation: 'enabled' }
     ),
     StoreModule.forRoot(
-      {},
+      {profileList : ProfileListReducer,
+      randomUser: RandomUserReducer},
       {
         metaReducers: !environment.production ? [] : [],
         runtimeChecks: {
@@ -46,7 +62,7 @@ import { InfoComponent } from './info.component';
         }
       }
     ),
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot([ProfileEffects,ProfileListEffects]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     BrowserAnimationsModule
   ],
