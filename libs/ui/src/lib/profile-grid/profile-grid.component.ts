a188e79dfc9105d2ed51e7941f51bed58405ef3c
Kanchan Barsainya <kanchan.barsainya@cvscaremark.com>
2020-03-07 13:15:31 -0500

    added event emitter in profile grid presentation component to notify parent component of selected user from the list.

--- libs/ui/src/lib/profile-grid/profile-grid.component.ts
+++ libs/ui/src/lib/profile-grid/profile-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { UserProfile } from '../../../../feature-profile-details/src/lib/models';
 
 @Component({
@@ -10,12 +10,17 @@ import { UserProfile } from '../../../../feature-profile-details/src/lib/models'
 export class ProfileGridComponent {
   @Input() users: UserProfile[];
 
+  @Output() userSelection = new EventEmitter<number>();
+
+
   displayedColumns: string[] = ['pictureUrl', 'name', 'email'];
 
   constructor() { }
 
-  goToProfile() {
+  goToProfile(profileIndex) {
     // Write code to navigate to the profile details page
+    /*emitting event to parent component [ProfileGridListComponent] when a user is selected and passing the index which
+    will be used as query param in route */
+    this.userSelection.next(profileIndex);
   }
-
 }
